5e1686e379e8df3389b8eafcc4bd8e8abdad7277
Roshan Narayan <rnarayan7@users.noreply.github.com>
2018-11-25 22:44:23 -0500

    Initial Commit

---
+++ AR+ParibusTests/AR_ParibusTests.swift
@@ -0,0 +1,34 @@
+//
+//  AR_ParibusTests.swift
+//  AR+ParibusTests
+//
+//  Created by Roshan Narayan on 11/25/18.
+//  Copyright © 2018 Roshan Narayan. All rights reserved.
+//
+
+import XCTest
+@testable import AR_Paribus
+
+class AR_ParibusTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
